3a3a3acb2c97f301e39d64adeeb3cd0d26be6cb5
goose <53491740+UndercoverGoose@users.noreply.github.com>
2025-05-29 21:55:50 -0400

    feat: main gui system

---
+++ src/main/java/io/github/waqfs/gui/CigaretteScreen.java
@@ -0,0 +1,109 @@
+package io.github.waqfs.gui;
+
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
+import org.lwjgl.glfw.GLFW;
+
+public class CigaretteScreen extends Screen {
+    private Screen parent = null;
+
+    protected CigaretteScreen() {
+        super(Text.literal("Cigarette Client"));
+    }
+
+    public void setParent(@Nullable Screen parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    protected void init() {
+//        ScrollableWidget<ClickableWidget> category1 = new ScrollableWidget<>(10, 10).setHeader(Text.literal("Category 1"));
+//        ScrollableWidget<ClickableWidget> category2 = new ScrollableWidget<>(120, 10).setHeader(Text.literal("Category 2"));
+//
+//        ToggleOptionsWidget toggle1 = new ToggleOptionsWidget(Text.literal("Toggle 1"), Text.literal("cigarette"));
+//        ToggleOptionsWidget toggle2 = new ToggleOptionsWidget(Text.literal("Toggle 2"), Text.literal("client"));
+//        ToggleOptionsWidget toggle3 = new ToggleOptionsWidget(Text.literal("Toggle 3"));
+//
+//        ToggleOptionsWidget option1 = new ToggleOptionsWidget(Text.literal("Option 1"), Text.literal("otp1"));
+//        ToggleOptionsWidget option2 = new ToggleOptionsWidget(Text.literal("Option 2"), Text.literal("otp2"));
+//        ToggleOptionsWidget option3 = new ToggleOptionsWidget(Text.literal("Option 3"), Text.literal("otp3"));
+//        ToggleOptionsWidget option4 = new ToggleOptionsWidget(Text.literal("Option 4"), Text.literal("otp4"));
+//        ToggleOptionsWidget option5 = new ToggleOptionsWidget(Text.literal("Option 5"), Text.literal("otp5"));
+//        ToggleOptionsWidget option6 = new ToggleOptionsWidget(Text.literal("Option 6"), Text.literal("otp6"));
+//        ToggleOptionsWidget option7 = new ToggleOptionsWidget(Text.literal("Option 7"), Text.literal("otp7"));
+//        ToggleOptionsWidget option8 = new ToggleOptionsWidget(Text.literal("Option 8"), Text.literal("otp8"));
+//        ToggleOptionsWidget option9 = new ToggleOptionsWidget(Text.literal("Option 9"), Text.literal("otp9"));
+//        ToggleOptionsWidget option0 = new ToggleOptionsWidget(Text.literal("Option 0"), Text.literal("otp0"));
+//        ToggleOptionsWidget option11 = new ToggleOptionsWidget(Text.literal("Option 8"), Text.literal("otp8"));
+//        ToggleOptionsWidget option12 = new ToggleOptionsWidget(Text.literal("Option 9"), Text.literal("otp9"));
+//        ToggleOptionsWidget option13 = new ToggleOptionsWidget(Text.literal("Option 0"), Text.literal("otp0"));
+//
+//        toggle1.setOptions(option1, option2);
+//        toggle3.setOptions(option3, option4, option5, option6, option7, option8, option9, option0, option11, option12, option13);
+//        category1.setChildren(toggle1, toggle2);
+//        category2.setChildren(toggle3);
+//
+//        addDrawableChild(category1);
+//        addDrawableChild(category2);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        for (Element child : this.children()) {
+            boolean handled = child.mouseClicked(mouseX, mouseY, button);
+            if (handled) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double horizontalAmount, double verticalAmount) {
+        for (Element child : this.children()) {
+            boolean handled = child.mouseScrolled(mouseX, mouseY, horizontalAmount, verticalAmount);
+            if (handled) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseDragged(double x, double y, int button, double deltaX, double deltaY) {
+        for (Element child : this.children()) {
+            boolean handled = child.mouseDragged(x, y, button, deltaX, deltaY);
+            if (handled) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        for (Element child : this.children()) {
+            boolean handled = child.mouseReleased(mouseX, mouseY, button);
+            if (handled) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void mouseMoved(double mouseX, double mouseY) {
+        for (Element child : this.children()) {
+            child.mouseMoved(mouseX, mouseY);
+        }
+    }
+
+
+    @Override
+    public void close() {
+        assert client != null;
+        client.setScreen(parent);
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        switch (keyCode) {
+            case GLFW.GLFW_KEY_ESCAPE, GLFW.GLFW_KEY_RIGHT_SHIFT -> this.close();
+        }
+        return true;
+    }
+}
---
+++ src/main/java/io/github/waqfs/gui/widget/PassthroughWidget.java
@@ -0,0 +1,105 @@
+package io.github.waqfs.gui.widget;
+
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
+
+public abstract class PassthroughWidget<T extends Element> extends ClickableWidget {
+    protected @Nullable T[] children;
+
+    public PassthroughWidget(int x, int y, int width, int height, Text message) {
+        super(x, y, width, height, message);
+    }
+
+    @Override
+    public boolean isMouseOver(double mouseX, double mouseY) {
+        return mouseX > this.getX() && mouseX < this.getX() + this.width && mouseY > this.getY() && mouseY < this.getY() + this.height;
+    }
+
+    @Override
+    public void mouseMoved(double mouseX, double mouseY) {
+        if (children == null) return;
+        for (Element child : children) {
+            if (child == null) continue;
+            child.mouseMoved(mouseX, mouseY);
+        }
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (children == null) return false;
+        for (Element child : children) {
+            if (child == null) continue;
+            boolean handled = child.mouseClicked(mouseX, mouseY, button);
+            if (handled) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseReleased(double mouseX, double mouseY, int button) {
+        if (children == null) return false;
+        for (Element child : children) {
+            if (child == null) continue;
+            boolean handled = child.mouseReleased(mouseX, mouseY, button);
+            if (handled) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
+        if (children == null) return false;
+        for (Element child : children) {
+            if (child == null) continue;
+            boolean handled = child.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
+            if (handled) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double horizontalAmount, double verticalAmount) {
+        if (children == null) return false;
+        for (Element child : children) {
+            if (child == null) continue;
+            boolean handled = child.mouseScrolled(mouseX, mouseY, horizontalAmount, verticalAmount);
+            if (handled) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (children == null) return false;
+        for (Element child : children) {
+            if (child == null) continue;
+            boolean handled = child.keyPressed(keyCode, scanCode, modifiers);
+            if (handled) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyReleased(int keyCode, int scanCode, int modifiers) {
+        if (children == null) return false;
+        for (Element child : children) {
+            if (child == null) continue;
+            boolean handled = child.keyReleased(keyCode, scanCode, modifiers);
+            if (handled) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean charTyped(char chr, int modifiers) {
+        if (children == null) return false;
+        for (Element child : children) {
+            if (child == null) continue;
+            boolean handled = child.charTyped(chr, modifiers);
+            if (handled) return true;
+        }
+        return false;
+    }
+}

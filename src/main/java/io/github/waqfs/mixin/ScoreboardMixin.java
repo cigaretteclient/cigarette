b41ca0fd370583a3b7cfdcbeed272724ef3e3598
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-07-18 12:15:32 -0400

    fix: game detection not running with static header

--- src/main/java/io/github/waqfs/mixin/ScoreboardMixin.java
+++ src/main/java/io/github/waqfs/mixin/ScoreboardMixin.java
@@ -3,6 +3,7 @@ package io.github.waqfs.mixin;
 import io.github.waqfs.GameDetector;
 import net.minecraft.scoreboard.Scoreboard;
 import net.minecraft.scoreboard.ScoreboardObjective;
+import net.minecraft.scoreboard.Team;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,6 +11,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Scoreboard.class)
 public class ScoreboardMixin {
+    @Inject(method = "updateScoreboardTeam", at = @At("HEAD"))
+    private void updateScoreboardTeam(Team team, CallbackInfo info) {
+        GameDetector.detect();
+    }
+
     @Inject(method = "updateExistingObjective", at = @At("HEAD"))
     private void updateExistingObjective(ScoreboardObjective objective, CallbackInfo info) {
         GameDetector.detect();

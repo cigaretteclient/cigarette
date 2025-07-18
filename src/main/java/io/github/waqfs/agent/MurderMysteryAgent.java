14fa81d0aa2e5b96fd1b27bb32501b09e36b135e
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-07-18 14:22:25 -0400

    feat: murder mystery agent (#4)
    * feat: murder mystery agent and persistent state for roles
    
    * use murder mystery agent
    
    * persist the entire player class instead of just roles

---
+++ src/main/java/io/github/waqfs/agent/MurderMysteryAgent.java
@@ -0,0 +1,180 @@
+package io.github.waqfs.agent;
+
+import io.github.waqfs.GameDetector;
+import io.github.waqfs.lib.PlayerEntityL;
+import io.github.waqfs.lib.TextL;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.UUID;
+
+public class MurderMysteryAgent implements ClientModInitializer {
+    private static final HashMap<String, PersistentPlayer> persistentPlayers = new HashMap<>();
+    private static final HashSet<AvailableGold> availableGold = new HashSet<>();
+
+    @Override
+    public void onInitializeClient() {
+        ClientTickEvents.START_CLIENT_TICK.register(client -> {
+            if (client.world == null || GameDetector.rootGame != GameDetector.ParentGame.MURDER_MYSTERY || GameDetector.subGame == GameDetector.ChildGame.NULL) {
+                this.unset();
+                return;
+            }
+
+            this.cleanupAvailableGold();
+
+            for (Entity entity : client.world.getEntities()) {
+                if (entity instanceof PlayerEntity player) {
+                    PersistentPlayer existingPlayer = this.getOrCreatePersistentPlayer(player);
+
+                    ItemStack item = PlayerEntityL.getHeldItem(player);
+                    if (item == null) continue;
+                    if (this.isNeutralItem(item)) continue;
+                    if (this.isDetectiveItem(item)) {
+                        existingPlayer.setDetective();
+                        continue;
+                    }
+
+                    String itemName = TextL.toColorCodedString(item.getFormattedName());
+                    if (itemName.startsWith("§r§a")) {
+                        existingPlayer.setMurderer();
+                    }
+                    continue;
+                }
+                if (entity instanceof ItemEntity item) {
+                    ItemStack stack = item.getStack();
+                    if (stack.isOf(Items.GOLD_INGOT)) {
+                        if (this.goldExists(item)) continue;
+                        this.createGold(item);
+                        continue;
+                    }
+                }
+            }
+        });
+    }
+
+    public static HashSet<PersistentPlayer> getVisiblePlayers() {
+        HashSet<PersistentPlayer> visiblePlayers = new HashSet<>();
+        for (PersistentPlayer player : persistentPlayers.values()) {
+            if (!player.playerEntity.isAlive()) continue;
+            visiblePlayers.add(player);
+        }
+        return visiblePlayers;
+    }
+
+    public static HashSet<AvailableGold> getVisibleGold() {
+        return new HashSet<>(availableGold);
+    }
+
+    private void cleanupAvailableGold() {
+        HashSet<AvailableGold> scheduleGold = new HashSet<>();
+        for (AvailableGold gold : availableGold) {
+            if (gold.itemEntity.isAlive()) continue;
+            scheduleGold.add(gold);
+        }
+        for (AvailableGold gold : scheduleGold) {
+            availableGold.remove(gold);
+        }
+    }
+
+    private boolean isNeutralItem(ItemStack item) {
+        if (item.isOf(Items.FILLED_MAP)) return true;
+        if (item.isOf(Items.GOLD_INGOT)) return true;
+        if (item.isOf(Items.ARMOR_STAND)) return true;
+        if (item.isOf(Items.FIREWORK_STAR)) return true;
+        if (item.isOf(Items.BLUE_STAINED_GLASS)) return true;
+        if (item.isOf(Items.COMPASS)) return true;
+        if (item.isOf(Items.COMPARATOR)) return true;
+        if (item.isOf(Items.RED_BED)) return true;
+        return false;
+    }
+
+    private boolean isDetectiveItem(ItemStack item) {
+        if (item.isOf(Items.ARROW)) return true;
+        if (item.isOf(Items.BOW)) return true;
+        return false;
+    }
+
+    private PersistentPlayer getOrCreatePersistentPlayer(PlayerEntity player) {
+        String playerName = player.getNameForScoreboard();
+        PersistentPlayer persistPlayer = persistentPlayers.get(playerName);
+        if (persistPlayer == null) {
+            persistPlayer = new PersistentPlayer(player);
+            persistentPlayers.put(playerName, persistPlayer);
+        }
+        persistPlayer.setPlayerEntity(player);
+        return persistPlayer;
+    }
+
+    private boolean goldExists(ItemEntity item) {
+        UUID uuid = item.getUuid();
+        for (AvailableGold gold : availableGold) {
+            if (gold.uuid == uuid) return true;
+        }
+        return false;
+    }
+
+    private void createGold(ItemEntity item) {
+        AvailableGold gold = new AvailableGold(item);
+        availableGold.add(gold);
+    }
+
+    private void unset() {
+        persistentPlayers.clear();
+        availableGold.clear();
+    }
+
+    public static class PersistentPlayer {
+        public PlayerEntity playerEntity;
+        public final String name;
+        public Role role;
+
+        public PersistentPlayer(PlayerEntity playerEntity) {
+            this.playerEntity = playerEntity;
+            this.name = playerEntity.getNameForScoreboard();
+            this.role = Role.INNOCENT;
+        }
+
+        protected void setPlayerEntity(PlayerEntity playerEntity) {
+            this.playerEntity = playerEntity;
+        }
+
+        public void setMurderer() {
+            this.role = Role.MURDERER;
+        }
+
+        public void setDetective() {
+            if (this.role == Role.MURDERER) return;
+            this.role = Role.DETECTIVE;
+        }
+
+        public enum Role {
+            INNOCENT(0), MURDERER(1), DETECTIVE(2);
+            private final int id;
+
+            Role(int id) {
+                this.id = id;
+            }
+
+            public int getId() {
+                return id;
+            }
+        }
+    }
+
+    public static class AvailableGold {
+        public final ItemEntity itemEntity;
+        public final UUID uuid;
+
+        public AvailableGold(ItemEntity itemEntity) {
+            this.itemEntity = itemEntity;
+            this.uuid = itemEntity.getUuid();
+        }
+    }
+}
--- src/main/java/io/github/waqfs/module/murdermystery/GoldEsp.java
+++ src/main/java/io/github/waqfs/module/murdermystery/GoldEsp.java
@@ -1,14 +1,12 @@
 package io.github.waqfs.module.murdermystery;
 
 import io.github.waqfs.GameDetector;
+import io.github.waqfs.agent.MurderMysteryAgent;
 import io.github.waqfs.lib.Glow;
 import io.github.waqfs.module.BaseModule;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.ItemEntity;
-import net.minecraft.item.Items;
 
-import java.util.UUID;
+import java.util.HashSet;
 
 public class GoldEsp extends BaseModule {
     protected static final String MODULE_NAME = "GoldESP";
@@ -18,18 +16,15 @@ public class GoldEsp extends BaseModule {
 
     public GoldEsp() {
         super(MODULE_ID, MODULE_NAME, MODULE_TOOLTIP);
+        this.glowContext.removeAll();
         ClientTickEvents.START_CLIENT_TICK.register(client -> {
-            if (!this.isEnabled() || GameDetector.rootGame != GameDetector.ParentGame.MURDER_MYSTERY || GameDetector.subGame == GameDetector.ChildGame.NULL) {
-                this.glowContext.removeAll();
+            if (!this.isEnabled() || GameDetector.rootGame != GameDetector.ParentGame.MURDER_MYSTERY || GameDetector.subGame == GameDetector.ChildGame.NULL)
                 return;
-            }
             if (client.world == null) return;
-            for (Entity entity : client.world.getEntities()) {
-                if (!(entity instanceof ItemEntity item)) continue;
-                if (item.getStack().isOf(Items.GOLD_INGOT)) {
-                    UUID uuid = item.getUuid();
-                    this.glowContext.addGlow(uuid, 0xFFD800);
-                }
+
+            HashSet<MurderMysteryAgent.AvailableGold> availableGold = MurderMysteryAgent.getVisibleGold();
+            for (MurderMysteryAgent.AvailableGold gold : availableGold) {
+                this.glowContext.addGlow(gold.uuid, 0xFFD800);
             }
         });
     }

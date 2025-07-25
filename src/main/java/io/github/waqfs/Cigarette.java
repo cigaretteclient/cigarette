1d3707dbdeeff21f2b00bb8ee8b386760d336fe6
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-07-25 10:17:53 -0400

    remove client mod initializers and mimic module structure

--- src/main/java/io/github/waqfs/Cigarette.java
+++ src/main/java/io/github/waqfs/Cigarette.java
@@ -1,5 +1,7 @@
 package io.github.waqfs;
 
+import io.github.waqfs.agent.BedwarsAgent;
+import io.github.waqfs.agent.MurderMysteryAgent;
 import io.github.waqfs.config.Config;
 import io.github.waqfs.config.FileSystem;
 import io.github.waqfs.lib.ChatLogger;
@@ -9,14 +11,11 @@ import org.slf4j.LoggerFactory;
 
 public class Cigarette implements ModInitializer {
     public static final String MOD_ID = "cigarette";
-
-    // This logger is used to write text to the console and the log file.
-    // It is considered best practice to use your mod id as the logger's name.
-    // That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static final ChatLogger CHAT_LOGGER = new ChatLogger();
-
     public static Config CONFIG = new Config();
+    public static BedwarsAgent BEDWARS_AGENT = new BedwarsAgent();
+    public static MurderMysteryAgent MURDER_MYSTERY_AGENT = new MurderMysteryAgent();
 
     @Override
     public void onInitialize() {

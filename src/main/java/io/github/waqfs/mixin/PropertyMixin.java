4e6f8ba674db055389e41bdfce39263f8434cb59
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-07-31 23:01:09 -0400

    fix: signature validation error

---
+++ src/main/java/io/github/waqfs/mixin/PropertyMixin.java
@@ -0,0 +1,14 @@
+package io.github.waqfs.mixin;
+
+import com.mojang.authlib.properties.Property;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Property.class)
+public class PropertyMixin {
+    @Nullable String signature;
+
+    public boolean hasSignature() {
+        return signature != null && signature.length() > 0;
+    }
+}

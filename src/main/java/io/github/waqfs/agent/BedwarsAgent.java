decbc68730c07ca4f651f544a63981170e495c3f
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-08-02 20:44:24 -0400

    refactor: move block state checking into agent

--- src/main/java/io/github/waqfs/agent/BedwarsAgent.java
+++ src/main/java/io/github/waqfs/agent/BedwarsAgent.java
@@ -5,6 +5,7 @@ import io.github.waqfs.module.bedwars.AutoClicker;
 import io.github.waqfs.module.bedwars.PerfectHit;
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.enums.BedPart;
 import net.minecraft.client.MinecraftClient;
@@ -51,6 +52,30 @@ public class BedwarsAgent extends BaseAgent {
 
         BlockPos otherBedPos = isHead ? pos.subtract(facing.getVector()) : pos.add(facing.getVector());
         persistentBeds.add(new PersistentBed(isHead ? pos : otherBedPos, facing, isFoot ? pos : otherBedPos, facing.getOpposite()));
+    }
+
+    public static boolean isWool(BlockState state) {
+        return state.isOf(Blocks.WHITE_WOOL) || state.isOf(Blocks.BLACK_WOOL) || state.isOf(Blocks.BLUE_WOOL) || state.isOf(Blocks.BROWN_WOOL) || state.isOf(Blocks.CYAN_WOOL) || state.isOf(Blocks.GRAY_WOOL) || state.isOf(Blocks.GREEN_WOOL) || state.isOf(Blocks.LIGHT_BLUE_WOOL) || state.isOf(Blocks.LIGHT_GRAY_WOOL) || state.isOf(Blocks.LIME_WOOL) || state.isOf(Blocks.MAGENTA_WOOL) || state.isOf(Blocks.ORANGE_WOOL) || state.isOf(Blocks.PINK_WOOL) || state.isOf(Blocks.PURPLE_WOOL) || state.isOf(Blocks.RED_WOOL) || state.isOf(Blocks.YELLOW_WOOL);
+    }
+
+    public static boolean isEndStone(BlockState state) {
+        return state.isOf(Blocks.END_STONE);
+    }
+
+    public static boolean isWood(BlockState state) {
+        return state.isOf(Blocks.OAK_PLANKS) || state.isOf(Blocks.ACACIA_PLANKS) || state.isOf(Blocks.SPRUCE_PLANKS) || state.isOf(Blocks.DARK_OAK_PLANKS) || state.isOf(Blocks.BIRCH_PLANKS) || state.isOf(Blocks.ACACIA_LOG) || state.isOf(Blocks.BIRCH_LOG) || state.isOf(Blocks.DARK_OAK_LOG) || state.isOf(Blocks.JUNGLE_LOG) || state.isOf(Blocks.OAK_LOG) || state.isOf(Blocks.SPRUCE_LOG);
+    }
+
+    public static boolean isClay(BlockState state) {
+        return state.isOf(Blocks.TERRACOTTA) || state.isOf(Blocks.BLACK_TERRACOTTA) || state.isOf(Blocks.BLUE_TERRACOTTA) || state.isOf(Blocks.BROWN_TERRACOTTA) || state.isOf(Blocks.CYAN_TERRACOTTA) || state.isOf(Blocks.GRAY_TERRACOTTA) || state.isOf(Blocks.GREEN_TERRACOTTA) || state.isOf(Blocks.LIGHT_BLUE_TERRACOTTA) || state.isOf(Blocks.LIGHT_GRAY_TERRACOTTA) || state.isOf(Blocks.LIME_TERRACOTTA) || state.isOf(Blocks.MAGENTA_TERRACOTTA) || state.isOf(Blocks.ORANGE_TERRACOTTA) || state.isOf(Blocks.PINK_TERRACOTTA) || state.isOf(Blocks.PURPLE_TERRACOTTA) || state.isOf(Blocks.RED_TERRACOTTA) || state.isOf(Blocks.WHITE_TERRACOTTA) || state.isOf(Blocks.YELLOW_TERRACOTTA);
+    }
+
+    public static boolean isObsidian(BlockState state) {
+        return state.isOf(Blocks.OBSIDIAN);
+    }
+
+    public static boolean isGlass(BlockState state) {
+        return state.isOf(Blocks.BLACK_STAINED_GLASS) || state.isOf(Blocks.BLUE_STAINED_GLASS) || state.isOf(Blocks.BROWN_STAINED_GLASS) || state.isOf(Blocks.CYAN_STAINED_GLASS) || state.isOf(Blocks.GRAY_STAINED_GLASS) || state.isOf(Blocks.GREEN_STAINED_GLASS) || state.isOf(Blocks.LIGHT_BLUE_STAINED_GLASS) || state.isOf(Blocks.LIGHT_GRAY_STAINED_GLASS) || state.isOf(Blocks.LIME_STAINED_GLASS) || state.isOf(Blocks.MAGENTA_STAINED_GLASS) || state.isOf(Blocks.ORANGE_STAINED_GLASS) || state.isOf(Blocks.PINK_STAINED_GLASS) || state.isOf(Blocks.PURPLE_STAINED_GLASS) || state.isOf(Blocks.RED_STAINED_GLASS) || state.isOf(Blocks.WHITE_STAINED_GLASS) || state.isOf(Blocks.YELLOW_STAINED_GLASS);
     }
 
     @Override

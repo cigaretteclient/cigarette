75b53a399ae96a7c9ff904cb4cee5215aa2fa3fd
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-08-04 19:11:37 -0400

    feat: zombies playeresp

--- src/main/java/io/github/waqfs/config/Config.java
+++ src/main/java/io/github/waqfs/config/Config.java
@@ -11,12 +11,14 @@ public class Config {
     public Category keybinds = new Category("Keybinds", 10, 10);
     public Category murderMystery = new Category("Murder Mystery", 120, 10);
     public Category bedwars = new Category("Bed Wars", 230, 10);
+    public Category zombies = new Category("Zombies", 340, 10);
 
-    public Category[] allCategories = new Category[]{keybinds, murderMystery, bedwars};
+    public Category[] allCategories = new Category[]{keybinds, murderMystery, bedwars, zombies};
 
     public Config() {
         this.keybinds.attach(new AddGlassBlock(), new BreakBlock(), new VClip());
         this.murderMystery.attach(new io.github.waqfs.module.murdermystery.PlayerESP(), new GoldESP());
         this.bedwars.attach(new io.github.waqfs.module.bedwars.PlayerESP(), new FireballESP(), new ProjectileESP(), new EntityESP(), new DefenseViewer(), new JumpReset(), new AutoClicker(), new PerfectHit(), new AutoTool());
+        this.zombies.attach(new io.github.waqfs.module.zombies.PlayerESP());
     }
 }

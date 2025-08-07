677322ad22db41ac96a889950152c60b1e091e5e
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-08-07 12:27:36 -0400

    use ToggleColorWidget as root widget

--- src/main/java/io/github/waqfs/module/zombies/PlayerESP.java
+++ src/main/java/io/github/waqfs/module/zombies/PlayerESP.java
@@ -1,8 +1,7 @@
 package io.github.waqfs.module.zombies;
 
 import io.github.waqfs.GameDetector;
-import io.github.waqfs.gui.widget.ColorPickerWidget;
-import io.github.waqfs.gui.widget.ToggleOptionsWidget;
+import io.github.waqfs.gui.widget.ToggleColorWidget;
 import io.github.waqfs.lib.Glow;
 import io.github.waqfs.lib.WorldL;
 import io.github.waqfs.module.TickModule;
@@ -10,22 +9,18 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
-public class PlayerESP extends TickModule<ToggleOptionsWidget> {
+public class PlayerESP extends TickModule<ToggleColorWidget> {
     protected static final String MODULE_NAME = "PlayerESP";
     protected static final String MODULE_TOOLTIP = "Highlights all the players in the game.";
     protected static final String MODULE_ID = "zombies.playeresp";
     private final Glow.Context glowContext = new Glow.Context();
-    private final ColorPickerWidget color = new ColorPickerWidget(Text.literal("Color"), false).withDefaultColor(0xFFFCF805);
 
     public PlayerESP() {
-        super(ToggleOptionsWidget.base, MODULE_ID, MODULE_NAME, MODULE_TOOLTIP);
-        this.widget.setOptions(color);
-        color.registerAsOption("zombies.playeresp.color");
+        super(ToggleColorWidget.base, MODULE_ID, MODULE_NAME, MODULE_TOOLTIP);
     }
 
     @Override
@@ -33,7 +28,7 @@ public class PlayerESP extends TickModule<ToggleOptionsWidget> {
         this.glowContext.removeAll();
         for (PlayerEntity playerEntity : WorldL.getRealPlayers()) {
             UUID uuid = playerEntity.getUuid();
-            this.glowContext.addGlow(uuid, color.getStateRGB());
+            this.glowContext.addGlow(uuid, this.widget.getStateRGB());
         }
     }
 

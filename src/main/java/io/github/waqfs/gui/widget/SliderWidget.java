1e2c1243304ade3b0bff87355a5838c21b96639e
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-08-07 22:12:04 -0400

    fix: hover animations dont work

--- src/main/java/io/github/waqfs/gui/widget/SliderWidget.java
+++ src/main/java/io/github/waqfs/gui/widget/SliderWidget.java
@@ -126,8 +126,7 @@ public class SliderWidget extends ClickableWidget {
         int top = getY();
         int bottom = getBottom();
 
-        if (isMouseOver(mouseX, mouseY) && !CigaretteScreen.hoverHandled) {
-            CigaretteScreen.hoverHandled = true;
+        if (isMouseOver(mouseX, mouseY) && CigaretteScreen.isHoverable(this)) {
             context.fillGradient(left, top, right, bottom, CigaretteScreen.BACKGROUND_COLOR, CigaretteScreen.DARK_BACKGROUND_COLOR);
         } else {
             context.fill(left, top, right, bottom, CigaretteScreen.BACKGROUND_COLOR);

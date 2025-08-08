e40e9f5a21dfa25bcb1d214e7efc3333c98ef36a
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-08-08 14:17:03 -0400

    create dedicated toggle widget

---
+++ src/main/java/io/github/waqfs/gui/widget/ToggleWidget.java
@@ -0,0 +1,51 @@
+package io.github.waqfs.gui.widget;
+
+import io.github.waqfs.gui.CigaretteScreen;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
+import org.lwjgl.glfw.GLFW;
+
+public class ToggleWidget extends BaseWidget<Boolean> {
+    private static final int MAX_HOVER_TICKS = 35;
+    private int ticksOnHover = 0;
+
+    public ToggleWidget(Text message, @Nullable Text tooltip) {
+        super(message, tooltip);
+        this.captureHover().withDefault(false);
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        if (!isMouseOver(mouseX, mouseY)) return false;
+        if (button == GLFW.GLFW_MOUSE_BUTTON_LEFT) {
+            this.setRawState(!this.getRawState());
+        }
+        return true;
+    }
+
+
+    @Override
+    protected void render(DrawContext context, boolean hovered, int mouseX, int mouseY, float deltaTicks, int left, int top, int right, int bottom) {
+        if (hovered) {
+            ticksOnHover = Math.min(++ticksOnHover, MAX_HOVER_TICKS);
+            context.fillGradient(left, top, right, bottom, CigaretteScreen.BACKGROUND_COLOR, CigaretteScreen.DARK_BACKGROUND_COLOR);
+        } else {
+            ticksOnHover = Math.max(--ticksOnHover, 0);
+            context.fill(left, top, right, bottom, CigaretteScreen.BACKGROUND_COLOR);
+        }
+
+        int textColor = this.getRawState() ? CigaretteScreen.ENABLED_COLOR : CigaretteScreen.PRIMARY_TEXT_COLOR;
+
+        if (ticksOnHover > 0) {
+            float progress = (float) ticksOnHover / MAX_HOVER_TICKS;
+            context.drawHorizontalLine(left, (int) (left + width * progress), top, textColor);
+            context.drawHorizontalLine((int) (right - width * progress), right, bottom - 1, textColor);
+            context.drawVerticalLine(left, (int) (bottom - height * progress), bottom, textColor);
+            context.drawVerticalLine(right - 1, top, (int) (top + height * progress), textColor);
+        }
+
+        context.drawTextWithShadow(MinecraftClient.getInstance().textRenderer, getMessage(), left + 4, top + height / 3, textColor);
+    }
+}

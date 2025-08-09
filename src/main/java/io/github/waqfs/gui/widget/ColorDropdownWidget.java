c93e861e3ddc05b0b68d04e6672372b03a6e46fd
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-08-08 20:05:57 -0400

    create buildText method for creating color dropdowns without toggles

--- src/main/java/io/github/waqfs/gui/widget/ColorDropdownWidget.java
+++ src/main/java/io/github/waqfs/gui/widget/ColorDropdownWidget.java
@@ -5,7 +5,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
 
-public class ColorDropdownWidget<Widget extends ToggleWidget, StateType> extends DropdownWidget<Widget, StateType> {
+public class ColorDropdownWidget<Widget extends BaseWidget<StateType>, StateType> extends DropdownWidget<Widget, StateType> {
     private final ColorSquareWidget colorSquare = new ColorSquareWidget();
     private final SliderWidget sliderRed = new SliderWidget(Text.literal("Red")).withBounds(0, 255, 255);
     private final SliderWidget sliderGreen = new SliderWidget(Text.literal("Green")).withBounds(0, 255, 255);
@@ -32,7 +32,10 @@ public class ColorDropdownWidget<Widget extends ToggleWidget, StateType> extends
     }
 
     public boolean getToggleState() {
-        return this.header.getRawState();
+        if (this.header instanceof ToggleWidget) {
+            return ((ToggleWidget) this.header).getRawState();
+        }
+        throw new IllegalStateException("Cannot get boolean state on a stateless header in a dropdown widget.");
     }
 
     @SuppressWarnings("unchecked")
@@ -49,8 +52,8 @@ public class ColorDropdownWidget<Widget extends ToggleWidget, StateType> extends
         return this;
     }
 
-    public ColorDropdownWidget<Widget, StateType> withDefaultState(boolean state) {
-        this.header.withDefaultState(state);
+    public ColorDropdownWidget<Widget, StateType> withDefaultState(StateType state) {
+        this.header.withDefault(state);
         return this;
     }
 
@@ -98,7 +101,14 @@ public class ColorDropdownWidget<Widget extends ToggleWidget, StateType> extends
         wrapper.setHeader(widget);
         return wrapper;
     }
-    
+
+    public static ColorDropdownWidget<TextWidget, Stateless> buildText(Text displayName, @Nullable Text tooltip) {
+        ColorDropdownWidget<TextWidget, Stateless> wrapper = new ColorDropdownWidget<>(displayName, tooltip, true);
+        TextWidget widget = new TextWidget(displayName, tooltip);
+        wrapper.setHeader(widget);
+        return wrapper;
+    }
+
     @Override
     public void registerConfigKey(String key) {
         this.header.registerConfigKey(key);

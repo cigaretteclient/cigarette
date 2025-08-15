90ba35900f4a7677c7eec554503a2aebb3d027a4
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-08-15 09:36:35 -0400

    feat: toggle off team-based player esp and set a static color

--- src/main/java/io/github/waqfs/module/render/PlayerESP.java
+++ src/main/java/io/github/waqfs/module/render/PlayerESP.java
@@ -1,5 +1,8 @@
 package io.github.waqfs.module.render;
 
+import io.github.waqfs.gui.widget.BaseWidget;
+import io.github.waqfs.gui.widget.ColorDropdownWidget;
+import io.github.waqfs.gui.widget.TextWidget;
 import io.github.waqfs.gui.widget.ToggleWidget;
 import io.github.waqfs.lib.Glow;
 import io.github.waqfs.lib.WorldL;
@@ -8,6 +11,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.Text;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
@@ -16,10 +20,17 @@ public class PlayerESP extends TickModule<ToggleWidget, Boolean> {
     protected static final String MODULE_NAME = "PlayerESP";
     protected static final String MODULE_TOOLTIP = "Highlights all the players in the world.";
     protected static final String MODULE_ID = "render.playeresp";
+    private final ToggleWidget byTeamColor = new ToggleWidget(Text.literal("By Team Color"), Text.literal("ESP players in the color of their team.")).withDefaultState(true);
+    private final TextWidget nonTeamColorText = new TextWidget(Text.literal("Static Color"), Text.literal("The color of all players if coloring by team is disabled.")).centered(false);
+    private final ColorDropdownWidget<TextWidget, BaseWidget.Stateless> nonTeamColor = new ColorDropdownWidget<TextWidget, BaseWidget.Stateless>(Text.empty(), null).withAlpha(false).withDefaultColor(0xFFFFFFFF);
     private final Glow.Context glowContext = new Glow.Context();
 
     public PlayerESP() {
         super(ToggleWidget::module, MODULE_ID, MODULE_NAME, MODULE_TOOLTIP);
+        nonTeamColor.setHeader(nonTeamColorText);
+        this.setChildren(byTeamColor, nonTeamColor);
+        byTeamColor.registerConfigKey("render.playeresp.byteam");
+        nonTeamColor.registerConfigKey("render.playeresp.static");
     }
 
     @Override
@@ -27,8 +38,12 @@ public class PlayerESP extends TickModule<ToggleWidget, Boolean> {
         this.glowContext.removeAll();
         for (PlayerEntity playerEntity : WorldL.getRealPlayers()) {
             UUID uuid = playerEntity.getUuid();
-            int teamColor = playerEntity.getTeamColorValue();
-            this.glowContext.addGlow(uuid, teamColor);
+            if (byTeamColor.getRawState()) {
+                int teamColor = playerEntity.getTeamColorValue();
+                this.glowContext.addGlow(uuid, teamColor);
+            } else {
+                this.glowContext.addGlow(uuid, nonTeamColor.getStateRGB());
+            }
         }
     }
 

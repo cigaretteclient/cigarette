839834a868226ed1c59513e8f523614a85b2fde9
Syfe <22247810+ItsSyfe@users.noreply.github.com>
2025-08-18 13:30:39 +0100

    feat: revive aura mostly working

--- src/main/java/io/github/waqfs/module/zombies/ReviveAura.java
+++ src/main/java/io/github/waqfs/module/zombies/ReviveAura.java
@@ -2,14 +2,28 @@ package io.github.waqfs.module.zombies;
 
 import io.github.waqfs.GameDetector;
 import io.github.waqfs.gui.widget.ToggleWidget;
+import io.github.waqfs.mixin.ClientWorldAccessor;
+import io.github.waqfs.mixin.MinecraftClientInvoker;
 import io.github.waqfs.module.RenderModule;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.OtherClientPlayerEntity;
+import net.minecraft.client.network.PendingUpdateManager;
+import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityPose;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
 import org.jetbrains.annotations.NotNull;
 
 public class ReviveAura extends RenderModule<ToggleWidget, Boolean> {
@@ -37,10 +51,35 @@ public class ReviveAura extends RenderModule<ToggleWidget, Boolean> {
      */
     @Override
     protected void onEnabledTick(MinecraftClient client, @NotNull ClientWorld world, @NotNull ClientPlayerEntity player) {
+        final double reachSq = 3 * 3;
+        double closestDistSq = Double.MAX_VALUE;
+        OtherClientPlayerEntity target = null;
+
         for (Entity entity : world.getEntities()) {
-            if (!(entity instanceof ArmorStandEntity armorStandEntity)) continue;
-            System.out.println(armorStandEntity);
+            if (!(entity instanceof OtherClientPlayerEntity otherClientPlayerEntity)) continue;
+
+            EntityDimensions entityDimensions = otherClientPlayerEntity.getDimensions(otherClientPlayerEntity.getPose());
+
+            // check if a revive entity
+            if (entityDimensions.width() != 0.2f) continue;
+
+            // check if within range to hit
+            double distSq = player.squaredDistanceTo(otherClientPlayerEntity);
+
+            if (distSq < reachSq && distSq < closestDistSq) {
+                closestDistSq = distSq;
+                target = otherClientPlayerEntity;
+            }
         }
+
+        if (target == null) return;
+
+        // check that player is alive and not spectating
+        if (!player.isAlive() || player.isSpectator() || player.isInvisible()) return;
+
+        // DO NOT UNCOMMENT, THIS BANS YOU LOL
+        // player.networkHandler.sendPacket(PlayerInteractEntityC2SPacket.attack(target, player.isSneaking()));
+        System.out.println("Reviving " + target.getName().getString() + " at " + target.getPos());
     }
 
     @Override

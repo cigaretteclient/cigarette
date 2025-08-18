6d42c646a957f0b8d9eb7e84ff776b78d4fdf2cb
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-08-18 15:45:07 -0400

    render transparent ESP cube around all powerups

--- src/main/java/io/github/waqfs/module/zombies/PowerupESP.java
+++ src/main/java/io/github/waqfs/module/zombies/PowerupESP.java
@@ -1,65 +1,60 @@
 package io.github.waqfs.module.zombies;
 
+import com.mojang.blaze3d.vertex.VertexFormat;
 import io.github.waqfs.GameDetector;
+import io.github.waqfs.agent.ZombiesAgent;
 import io.github.waqfs.gui.widget.ToggleWidget;
+import io.github.waqfs.lib.Renderer;
 import io.github.waqfs.module.RenderModule;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.decoration.ArmorStandEntity;
-import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
+import org.joml.Matrix4f;
 
 import java.util.HashSet;
+import java.util.OptionalDouble;
 
 public class PowerupESP extends RenderModule<ToggleWidget, Boolean> {
     protected static final String MODULE_NAME = "PowerupESP";
     protected static final String MODULE_TOOLTIP = "Places ESP boxes around dropped powerups.";
     protected static final String MODULE_ID = "zombies.powerupesp";
-    private final HashSet<Powerup> powerups = new HashSet<>();
+    private static final RenderLayer RENDER_LAYER = RenderLayer.of("cigarette.blockesp", 1536, Renderer.BLOCK_ESP_PHASE, RenderLayer.MultiPhaseParameters.builder().lineWidth(new RenderPhase.LineWidth(OptionalDouble.of(1))).build(false));
 
     public PowerupESP() {
-        super(ToggleWidget::module, MODULE_NAME, MODULE_TOOLTIP, MODULE_ID);
+        super(ToggleWidget::module, MODULE_ID, MODULE_NAME, MODULE_TOOLTIP);
     }
 
     @Override
     protected void onWorldRender(WorldRenderContext ctx, @NotNull MatrixStack matrixStack) {
+        HashSet<ZombiesAgent.Powerup> powerups = ZombiesAgent.getPowerups();
+        if (powerups.size() == 0) return;
+
+        matrixStack.push();
+
+        Matrix4f matrix = Renderer.getCameraTranslatedMatrix(matrixStack, ctx);
+
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder buffer = tessellator.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+
+        for (ZombiesAgent.Powerup powerup : powerups) {
+            Renderer.drawCube(buffer, matrix, 0x3F000000 + powerup.type.getColor(), powerup.position, 2f);
+        }
+
+        BuiltBuffer built = buffer.endNullable();
+        if (built != null) RENDER_LAYER.draw(built);
+        matrixStack.pop();
     }
 
     @Override
     protected void onEnabledTick(MinecraftClient client, @NotNull ClientWorld world, @NotNull ClientPlayerEntity player) {
-        powerups.clear();
-        for (Entity entity : world.getEntities()) {
-            if (!(entity instanceof ArmorStandEntity armorStandEntity)) continue;
-        }
-    }
-
-    @Override
-    protected void onDisabledTick(MinecraftClient client) {
-        powerups.clear();
     }
 
     @Override
     public boolean inValidGame() {
         return GameDetector.rootGame == GameDetector.ParentGame.ZOMBIES && GameDetector.subGame == GameDetector.ChildGame.INSTANCED_ZOMBIES;
-    }
-
-    protected record Powerup(Vec3d position, Powerup.Type type) {
-        enum Type {
-            INSTANT_KILL(0xFFFF0000), MAX_AMMO(0xFF0000FF), DOUBLE_GOLD(0xFFFFF800);
-
-            private final int color;
-
-            Type(int argb) {
-                this.color = argb;
-            }
-
-            public int getColor() {
-                return this.color;
-            }
-        }
     }
 }

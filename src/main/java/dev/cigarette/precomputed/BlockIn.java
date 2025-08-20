01502f9745d7b3939981c535e2a7ff5e2a4ea2d3
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-08-20 11:06:05 -0400

    add precomputed neighbor vectors to iterate over

---
+++ src/main/java/dev/cigarette/precomputed/BlockIn.java
@@ -0,0 +1,8 @@
+package dev.cigarette.precomputed;
+
+import net.minecraft.util.math.Vec3i;
+
+public class BlockIn {
+    public static final Vec3i[] PLAYER_NEIGHBORS = new Vec3i[]{new Vec3i(1, 0, 0), new Vec3i(-1, 0, 0), new Vec3i(0, 0, 1), new Vec3i(0, 0, -1), new Vec3i(1, 1, 0), new Vec3i(-1, 1, 0), new Vec3i(0, 1, 1), new Vec3i(0, 1, -1), new Vec3i(0, 2, 0)};
+    public static final Vec3i[] BLOCK_NEIGHBORS = new Vec3i[]{new Vec3i(1, 0, 0), new Vec3i(-1, 0, 0), new Vec3i(0, 0, 1), new Vec3i(0, 0, -1), new Vec3i(0, 1, 0), new Vec3i(0, -1, 0)};
+}

92b9185f4151676766e763df7c0b81c68683436b
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-10-03 12:50:45 -0400

    create Minecraft Keyboard mixin

---
+++ src/main/java/dev/cigarette/mixin/KeyboardMixin.java
@@ -0,0 +1,25 @@
+package dev.cigarette.mixin;
+
+import dev.cigarette.helper.KeybindHelper;
+import net.minecraft.client.Keyboard;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Keyboard.class)
+public class KeyboardMixin {
+    @Inject(method = "onKey", at = @At("HEAD"), cancellable = true)
+    private void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (window != client.getWindow().getHandle()) {
+            ci.cancel();
+            return;
+        }
+        if (KeybindHelper.handleByGUI(client, key, scancode, action, modifiers) || KeybindHelper.handleBlockedInputs(client, key, scancode, action, modifiers) || KeybindHelper.handleCustom(client, key, scancode, action, modifiers)) {
+            ci.cancel();
+            return;
+        }
+    }
+}

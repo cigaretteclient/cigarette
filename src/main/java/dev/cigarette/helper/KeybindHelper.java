16d039d31ccb2600531eb54a756e68a01563b860
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-10-04 00:41:38 -0400

    use new wrappers

--- src/main/java/dev/cigarette/helper/KeybindHelper.java
+++ src/main/java/dev/cigarette/helper/KeybindHelper.java
@@ -2,17 +2,31 @@ package dev.cigarette.helper;
 
 import dev.cigarette.Cigarette;
 import dev.cigarette.gui.CigaretteScreen;
+import dev.cigarette.helper.keybind.InputBlocker;
+import dev.cigarette.helper.keybind.MinecraftKeybind;
+import dev.cigarette.helper.keybind.VirtualKeybind;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
+import org.jetbrains.annotations.Nullable;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.HashSet;
 
 public class KeybindHelper {
+    public static final MinecraftKeybind KEY_SNEAK = new MinecraftKeybind("key.sneak");
+    public static final MinecraftKeybind KEY_MOVE_BACK = new MinecraftKeybind("key.back");
+    public static final MinecraftKeybind KEY_MOVE_LEFT = new MinecraftKeybind("key.left");
+    public static final MinecraftKeybind KEY_MOVE_RIGHT = new MinecraftKeybind("key.right");
+    public static final MinecraftKeybind KEY_RIGHT_CLICK = new MinecraftKeybind("key.use");
+    public static final MinecraftKeybind KEY_JUMP = new MinecraftKeybind("key.jump");
+    /**
+     * Keybind to toggle {@code CigaretteScreen}.
+     */
+    public static final VirtualKeybind TOGGLE_GUI = new VirtualKeybind(GLFW.GLFW_KEY_RIGHT_SHIFT);
+
     /**
      * Set of custom keybinds that can be triggered.
      */
-    private static final HashSet<CigaretteKeyBind> customBinds = new HashSet<>();
+    private static final HashSet<VirtualKeybind> customBinds = new HashSet<>();
 
     /**
      * The module that is blocking inputs.
@@ -20,19 +34,9 @@ public class KeybindHelper {
     private static Object blockingModule = null;
 
     /**
-     * Set of keybinds that should be cancelled.
+     * The input blocker to pass events through.
      */
-    private static final HashSet<KeyBinding> blockedInputs = new HashSet<>();
-
-    /**
-     * Whether to block mouse movements from updating the players yaw/pitch.
-     */
-    private static boolean blockMouse = false;
-
-    /**
-     * Keybind to toggle {@code CigaretteScreen}.
-     */
-    public static final CigaretteKeyBind TOGGLE_GUI = new CigaretteKeyBind(GLFW.GLFW_KEY_RIGHT_SHIFT);
+    private static @Nullable InputBlocker blockedInputs = null;
 
     /**
      * Attempts to handle a key event inside the {@code CigaretteScreen} GUI.
@@ -53,7 +57,7 @@ public class KeybindHelper {
                 case GLFW.GLFW_RELEASE -> CigaretteScreen.bindingKey.keyReleased(key, scancode, modifiers);
             }
         } else {
-            if (key == GLFW.GLFW_KEY_ESCAPE || (action == GLFW.GLFW_PRESS && TOGGLE_GUI.matches(key))) {
+            if (key == GLFW.GLFW_KEY_ESCAPE || (action == GLFW.GLFW_PRESS && TOGGLE_GUI.isOf(key, scancode))) {
                 Cigarette.SCREEN.close();
             }
         }
@@ -72,13 +76,9 @@ public class KeybindHelper {
      * @return Whether the key event should be cancelled
      */
     public static boolean handleBlockedInputs(MinecraftClient client, int key, int scancode, int action, int modifiers) {
-        if (TOGGLE_GUI.matches(key)) return false;
-        for (KeyBinding keybind : blockedInputs) {
-            if (keybind.matchesKey(key, scancode)) {
-                return true;
-            }
-        }
-        return false;
+        if (TOGGLE_GUI.isOf(key, scancode)) return false;
+        if (blockedInputs == null) return false;
+        return blockedInputs.process(key, scancode, action, modifiers);
     }
 
     /**
@@ -94,14 +94,14 @@ public class KeybindHelper {
      */
     public static boolean handleCustom(MinecraftClient client, int key, int scancode, int action, int modifiers) {
         if (action != GLFW.GLFW_PRESS) return false;
-        if (TOGGLE_GUI.matches(key)) {
+        if (TOGGLE_GUI.isOf(key, scancode)) {
             Cigarette.SCREEN.setParent(client.currentScreen);
             client.setScreen(Cigarette.SCREEN);
             return true;
         }
-        for (CigaretteKeyBind binding : customBinds) {
-            if (!binding.matches(key)) continue;
-            binding.processAction(action);
+        for (VirtualKeybind binding : customBinds) {
+            if (!binding.isOf(key, scancode)) continue;
+            binding.physicalAction(action);
             return true;
         }
         return false;
@@ -110,33 +110,23 @@ public class KeybindHelper {
     /**
      * Attempts to start input blocking. {@return false if input blocking is already running} Use {@code forceBlockInputs()} to bypass this check.
      *
-     * @param keybindIds The keybind IDs to pass into {@code KeyBinding.byId()}
+     * @param module
+     * @param blocker
      */
-    public static boolean tryBlockInputs(Object module, String... keybindIds) {
-        if (!blockedInputs.isEmpty()) return false;
-        for (String id : keybindIds) {
-            KeyBinding keybind = KeyBinding.byId(id);
-            if (keybind == null) continue;
-            blockedInputs.add(keybind);
-        }
-        blockMouse = true;
-        blockingModule = module;
+    public static boolean tryBlockInputs(Object module, InputBlocker blocker) {
+        if (blockedInputs != null) return false;
+        forceBlockInputs(module, blocker);
         return true;
     }
 
     /**
      * Force starts input blocking with the provided configuration. Overrides any previously started configurations.
      *
-     * @param keybindIds The keybind IDs to pass into {@code KeyBinding.byId()}
+     * @param module
+     * @param blocker
      */
-    public static void forceBlockInputs(Object module, String... keybindIds) {
-        blockedInputs.clear();
-        for (String id : keybindIds) {
-            KeyBinding keybind = KeyBinding.byId(id);
-            if (keybind == null) continue;
-            blockedInputs.add(keybind);
-        }
-        blockMouse = true;
+    public static void forceBlockInputs(Object module, InputBlocker blocker) {
+        blockedInputs = blocker;
         blockingModule = module;
     }
 
@@ -144,8 +134,7 @@ public class KeybindHelper {
      * Disable input blocking.
      */
     public static void unblock() {
-        blockedInputs.clear();
-        blockMouse = false;
+        blockedInputs = null;
         blockingModule = null;
     }
 
@@ -156,108 +145,5 @@ public class KeybindHelper {
      */
     public static boolean isBlocking(Object module) {
         return blockingModule == module;
-    }
-
-    /**
-     * {@return whether the mouse is being input blocked}
-     */
-    public static boolean isMouseBlocked() {
-        return blockMouse;
-    }
-
-    /**
-     * A custom key bind used instead of the native {@code KeyBinding} class.
-     */
-    public static class CigaretteKeyBind {
-        /**
-         * The key code that triggers this keybind.
-         */
-        private int key;
-        /**
-         * The default key code set for this keybind.
-         */
-        private final int defaultKey;
-        /**
-         * Whether this key is currently pressed or not.
-         */
-        private boolean pressed = false;
-        /**
-         * The number of times this key has been pressed.
-         */
-        private int timesPressed = 0;
-
-        /**
-         * Creates a custom key bind.
-         *
-         * @param defaultKey The key and default key to use
-         */
-        public CigaretteKeyBind(int defaultKey) {
-            this.key = defaultKey;
-            this.defaultKey = defaultKey;
-            customBinds.add(this);
-        }
-
-        /**
-         * Updates {@code pressed} and {@code timesPressed} depending on the action that occurred on the key.
-         *
-         * @param glfwAction The key events action as defined by GLFW
-         */
-        protected void processAction(int glfwAction) {
-            switch (glfwAction) {
-                case GLFW.GLFW_PRESS -> {
-                    this.timesPressed++;
-                    this.pressed = true;
-                }
-                case GLFW.GLFW_RELEASE -> this.pressed = false;
-            }
-        }
-
-        /**
-         * {@return whether this keybind is currently pressed}
-         */
-        public boolean isPressed() {
-            return this.pressed;
-        }
-
-        /**
-         * {@return whether this keybind was pressed}
-         */
-        public boolean wasPressed() {
-            if (this.timesPressed == 0) return false;
-            this.timesPressed--;
-            return true;
-        }
-
-        /**
-         * {@return whether the input key matches the saved key code}
-         *
-         * @param key The key code to compare
-         */
-        public boolean matches(int key) {
-            return this.key == key;
-        }
-
-        /**
-         * {@return the key code that should trigger this key bind}
-         */
-        public int key() {
-            return this.key;
-        }
-
-        /**
-         * Sets the saved key.
-         *
-         * @param key The key code to save to this key bind
-         */
-        public void setKey(int key) {
-            this.key = key;
-        }
-
-        /**
-         * Resets this key bind to the default key.
-         */
-        public void reset() {
-            this.key = this.defaultKey;
-        }
     }
 }

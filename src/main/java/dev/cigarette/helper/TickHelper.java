2a28257c268433a8f6f61197305272c51f5034e5
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-10-08 08:56:09 -0400

    chore: add javadocs to TickHelper

--- src/main/java/dev/cigarette/helper/TickHelper.java
+++ src/main/java/dev/cigarette/helper/TickHelper.java
@@ -5,17 +5,41 @@ import net.minecraft.util.Pair;
 
 import java.util.HashMap;
 
+/**
+ * Helper class for scheduling events to occur in the future.
+ */
 public class TickHelper {
+    /**
+     * Actions that only occur once.
+     */
     private static final HashMap<Object, Pair<Integer, Runnable>> onceActions = new HashMap<>();
 
+    /**
+     * Schedule an {@code action} to occur after {@code ticks} number of ticks.
+     *
+     * @param key    A unique identifier to store this action. Usually {@code this} unless multiple actions are needed.
+     * @param action The action to perform once the number of ticks has passed.
+     * @param ticks  The number of ticks to wait until triggered.
+     */
     public static void scheduleOnce(Object key, Runnable action, int ticks) {
         onceActions.put(key, new Pair<>(ticks, action));
     }
 
+    /**
+     * Unschedule an event that was already scheduled.
+     *
+     * @param key The unique identifier used in scheduling the event.
+     */
     public static void unschedule(Object key) {
         onceActions.remove(key);
     }
 
+    /**
+     * Get the remaining ticks until a scheduled event triggers.
+     *
+     * @param key The unique identifier used in scheduling the event.
+     * @return Remaining ticks until event is triggered
+     */
     public static int whenOnce(Object key) {
         if (!onceActions.containsKey(key)) return -1;
         return onceActions.get(key).getLeft();

8c939ad130e197e92b944e89a9fdf091f9fcac55
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-11-28 16:29:51 -0500

    create skyblock category

--- src/main/java/dev/cigarette/config/Config.java
+++ src/main/java/dev/cigarette/config/Config.java
@@ -14,6 +14,7 @@ import dev.cigarette.module.keybind.VClip;
 import dev.cigarette.module.murdermystery.GoldESP;
 import dev.cigarette.module.murdermystery.PlayerESP;
 import dev.cigarette.module.render.ProjectileESP;
+import dev.cigarette.module.skyblock.RedGifter;
 import dev.cigarette.module.ui.*;
 import dev.cigarette.module.zombies.Aimbot;
 import dev.cigarette.module.zombies.PowerupESP;
@@ -74,6 +75,7 @@ public class Config {
         cfg.putModules("Render", dev.cigarette.module.render.PlayerESP.INSTANCE, ProjectileESP.INSTANCE);
         cfg.putModules("UI", GUI.INSTANCE, ModuleList.INSTANCE, Notifications.INSTANCE, TargetHUD.INSTANCE, Watermark.INSTANCE);
         cfg.putModules("Zombies", Aimbot.INSTANCE, PowerupESP.INSTANCE, ReviveAura.INSTANCE, ZombieESP.INSTANCE);
+        cfg.putModules("Skyblock", RedGifter.INSTANCE);
         if (Cigarette.IN_DEV_ENVIRONMENT) {
             cfg.putCategory("Agents", DevWidget.CATEGORY_INSTANCE);
         }

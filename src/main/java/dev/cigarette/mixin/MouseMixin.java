f5ef32bb4a538eeec0496995995dfa9fb92e019c
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-11-28 02:29:24 -0500

    fix: keybinds/mouse not unreleasing (#57)
    * Fixes mouse unrelease events being ignored while GUIs are open
    * Fixes keys desyncing after unblocking inputs resulting from virtual state not unsetting

--- src/main/java/dev/cigarette/mixin/MouseMixin.java
+++ src/main/java/dev/cigarette/mixin/MouseMixin.java
@@ -3,6 +3,7 @@ package dev.cigarette.mixin;
 import dev.cigarette.helper.KeybindHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
+import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -23,9 +24,13 @@ public class MouseMixin {
             ci.cancel();
             return;
         }
-        if (client.currentScreen == null && (KeybindHelper.handleBlockedMouseInputs(client, button, action, mods) || KeybindHelper.handleCustomMouse(client, button, action, mods))) {
-            ci.cancel();
-            return;
+        if (client.currentScreen == null) {
+            if (KeybindHelper.handleBlockedMouseInputs(client, button, action, mods) || KeybindHelper.handleCustomMouse(client, button, action, mods)) {
+                ci.cancel();
+                return;
+            }
+        } else if (action == GLFW.GLFW_RELEASE) {
+            KeybindHelper.handleCustomMouse(client, button, action, mods);
         }
     }
 }

d55614a4b444abcbdf7693aca354035434180e6f
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-12-01 01:18:50 -0500

    add automatic gift opening

--- src/main/java/dev/cigarette/module/skyblock/RedGifter.java
+++ src/main/java/dev/cigarette/module/skyblock/RedGifter.java
@@ -2,17 +2,54 @@ package dev.cigarette.module.skyblock;
 
 import dev.cigarette.GameDetector;
 import dev.cigarette.gui.widget.ToggleWidget;
+import dev.cigarette.lib.PlayerEntityL;
+import dev.cigarette.lib.TextL;
 import dev.cigarette.module.TickModule;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
 
 public class RedGifter extends TickModule<ToggleWidget, Boolean> {
     public static final RedGifter INSTANCE = new RedGifter("skyblock.redgifter", "Auto Red Gifter", "Automatically gives and opens red gifts.");
 
+    private final ToggleWidget gifter = new ToggleWidget("Run as Gifter", "Automatically gives red gifts to nearby players.").withDefaultState(false);
+    private final ToggleWidget opener = new ToggleWidget("Run as Opener", "Automatically opens red gifts you receive.").withDefaultState(true);
+
     private RedGifter(String id, String name, String tooltip) {
         super(ToggleWidget::module, id, name, tooltip);
+        this.setChildren(gifter, opener);
+        gifter.registerConfigKey(id + ".asgifter");
+        opener.registerConfigKey(id + ".asopener");
+        gifter.registerModuleCallback((Boolean state) -> {
+            if (opener.getRawState() == state) {
+                opener.setRawState(!state);
+            }
+        });
+        opener.registerModuleCallback((Boolean state) -> {
+            if (gifter.getRawState() == state) {
+                gifter.setRawState(!state);
+            }
+        });
     }
 
     @Override
     protected void onEnabledTick(net.minecraft.client.MinecraftClient client, net.minecraft.client.world.ClientWorld world, net.minecraft.client.network.ClientPlayerEntity player) {
+        if (opener.getRawState()) {
+            for (Entity entity : world.getOtherEntities(player, player.getBoundingBox().expand(4))) {
+                if (!(entity instanceof ArmorStandEntity armorStand)) continue;
+
+                Text customName = armorStand.getCustomName();
+                if (customName == null) continue;
+
+                String name = TextL.toColorCodedString(customName);
+                if (!name.equals("§r§e§lCLICK TO OPEN")) continue;
+
+                PlayerEntityL.setRotationVector(player, armorStand.getPos().add(0, 1.5, 0).subtract(player.getEyePos()));
+                if (client.interactionManager != null) client.interactionManager.interactEntity(player, armorStand, Hand.MAIN_HAND);
+            }
+
+        }
     }
 
     @Override

5bd21ac18500bf4a8ac7b05dc695ea08a5fb6387
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-12-03 19:44:03 -0500

    add automatic gift giving by player uuid

---
+++ src/main/java/dev/cigarette/mixin/MinecraftClientMixin.java
@@ -0,0 +1,32 @@
+package dev.cigarette.mixin;
+
+import dev.cigarette.lib.WorldL;
+import dev.cigarette.module.skyblock.RedGifter;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    private ClientPlayerEntity player;
+    private @Nullable HitResult crosshairTarget;
+
+    @Inject(method = "doItemUse", at = @At("HEAD"), cancellable = true)
+    private void doItemUse(CallbackInfo ci) {
+        if (RedGifter.INSTANCE.getRawState() && RedGifter.INSTANCE.inValidGame() && RedGifter.INSTANCE.gifter.getRawState() && RedGifter.isHoldingAGift() && this.crosshairTarget != null && this.crosshairTarget.getType() == HitResult.Type.ENTITY) {
+            Entity target = ((EntityHitResult) this.crosshairTarget).getEntity();
+            if (target instanceof PlayerEntity && WorldL.isRealPlayer((PlayerEntity) target)) {
+                ci.cancel();
+                RedGifter.INSTANCE.playerToGift = ((PlayerEntity) target).getGameProfile().getId();
+            }
+        }
+    }
+}
--- src/main/java/dev/cigarette/module/skyblock/RedGifter.java
+++ src/main/java/dev/cigarette/module/skyblock/RedGifter.java
@@ -4,21 +4,27 @@ import dev.cigarette.GameDetector;
 import dev.cigarette.gui.widget.ToggleWidget;
 import dev.cigarette.lib.PlayerEntityL;
 import dev.cigarette.lib.TextL;
+import dev.cigarette.lib.WorldL;
 import dev.cigarette.module.TickModule;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.UUID;
+
 public class RedGifter extends TickModule<ToggleWidget, Boolean> {
     public static final RedGifter INSTANCE = new RedGifter("skyblock.redgifter", "Auto Red Gifter", "Automatically gives and opens red gifts.");
 
-    private final ToggleWidget gifter = new ToggleWidget("Run as Gifter", "Automatically gives red gifts to nearby players.").withDefaultState(false);
-    private final ToggleWidget opener = new ToggleWidget("Run as Opener", "Automatically opens red gifts you receive.").withDefaultState(true);
+    public UUID playerToGift = null;
+    public final ToggleWidget gifter = new ToggleWidget("Run as Gifter", "Automatically gives red gifts to nearby players.").withDefaultState(false);
+    public final ToggleWidget opener = new ToggleWidget("Run as Opener", "Automatically opens red gifts you receive.").withDefaultState(true);
 
     private RedGifter(String id, String name, String tooltip) {
         super(ToggleWidget::module, id, name, tooltip);
@@ -37,6 +43,20 @@ public class RedGifter extends TickModule<ToggleWidget, Boolean> {
         });
     }
 
+    public static boolean isAGiftOfSomeSorts(ItemStack item) {
+        String itemName = TextL.toColorCodedString(item.getName());
+        return itemName.endsWith("Red Gift") || itemName.endsWith("White Gift") || itemName.endsWith("Green Gift");
+    }
+
+    public static boolean isHoldingAGift() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player != null) {
+            ItemStack mainHand = client.player.getStackInHand(Hand.MAIN_HAND);
+            return isAGiftOfSomeSorts(mainHand);
+        }
+        return false;
+    }
+
     @Override
     protected void onEnabledTick(@NotNull MinecraftClient client, @NotNull ClientWorld world, @NotNull ClientPlayerEntity player) {
         if (opener.getRawState()) {
@@ -50,10 +70,31 @@ public class RedGifter extends TickModule<ToggleWidget, Boolean> {
                 if (!name.equals("§r§e§lCLICK TO OPEN")) continue;
 
                 PlayerEntityL.setRotationVector(player, armorStand.getPos().add(0, 1.5, 0).subtract(player.getEyePos()));
-                if (client.interactionManager != null) client.interactionManager.interactEntity(player, armorStand, Hand.MAIN_HAND);
+                if (client.interactionManager != null) {
+                    client.interactionManager.interactEntity(player, armorStand, Hand.MAIN_HAND);
+                    client.interactionManager.interactEntity(player, armorStand, Hand.OFF_HAND);
+                }
             }
-
+        } else if (gifter.getRawState() && this.playerToGift != null) {
+            if (!RedGifter.isHoldingAGift()) {
+                return;
+            }
+            PlayerEntity targetPlayer = WorldL.getRealPlayerByUUID(this.playerToGift);
+            if (targetPlayer == null || player.distanceTo(targetPlayer) > 3) {
+                this.playerToGift = null;
+                return;
+            }
+            PlayerEntityL.setRotationVector(player, targetPlayer.getPos().add(0, 1, 0).subtract(player.getEyePos()));
+            if (client.interactionManager != null) {
+                client.interactionManager.interactEntity(player, targetPlayer, Hand.MAIN_HAND);
+                client.interactionManager.interactEntity(player, targetPlayer, Hand.OFF_HAND);
+            }
         }
+    }
+
+    @Override
+    protected void onDisabledTick(MinecraftClient client) {
+        this.playerToGift = null;
     }
 
     @Override

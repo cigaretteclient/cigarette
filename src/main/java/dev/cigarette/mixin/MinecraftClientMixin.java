0c3ef9c277e644a67efc2ff129a3d590b4d4ee48
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-12-04 21:28:47 -0500

    fix: crosshairTarget is always null outside dev

--- src/main/java/dev/cigarette/mixin/MinecraftClientMixin.java
+++ src/main/java/dev/cigarette/mixin/MinecraftClientMixin.java
@@ -10,14 +10,19 @@ import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
-    private ClientPlayerEntity player;
-    private @Nullable HitResult crosshairTarget;
+    @Shadow
+    public ClientPlayerEntity player;
+
+    @Shadow
+    @Nullable
+    public HitResult crosshairTarget;
 
     @Inject(method = "doItemUse", at = @At("HEAD"), cancellable = true)
     private void doItemUse(CallbackInfo ci) {

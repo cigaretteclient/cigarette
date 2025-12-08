039b63e8489af8afad2336a8cc16ee08b1faa0a5
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-12-08 13:12:35 -0500

    PlayerEntityHelper: add javadocs

--- src/main/java/dev/cigarette/helper/PlayerEntityHelper.java
+++ src/main/java/dev/cigarette/helper/PlayerEntityHelper.java
@@ -3,7 +3,16 @@ package dev.cigarette.helper;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Vec3d;
 
+/**
+ * Helper class for {@link PlayerEntity} related methods.
+ */
 public class PlayerEntityHelper {
+    /**
+     * Computes the rotation vector (yaw, pitch) required to look in the direction of the given vector.
+     *
+     * @param vector The direction vector.
+     * @return A float array where index 0 is yaw and index 1 is pitch.
+     */
     public static float[] getRotationVectorInDirection(Vec3d vector) {
         Vec3d normalized = vector.normalize();
         double pitchRadians = Math.asin(-normalized.y);
@@ -15,12 +24,27 @@ public class PlayerEntityHelper {
         return new float[]{yaw, pitch};
     }
 
+    /**
+     * Sets the player's rotation to look in the direction of the given vector.
+     *
+     * @param player The player entity.
+     * @param vector The direction vector.
+     */
     public static void setRotationVector(PlayerEntity player, Vec3d vector) {
         float[] yawPitch = getRotationVectorInDirection(vector);
         player.setYaw(yawPitch[0]);
         player.setPitch(yawPitch[1]);
     }
 
+    /**
+     * Calculates the angle difference between two sets of yaw and pitch angles
+     *
+     * @param yaw    The first yaw angle
+     * @param pitch  The first pitch angle
+     * @param yaw2   The second yaw angle
+     * @param pitch2 The second pitch angle
+     * @return The difference between the two sets of angles in degrees
+     */
     public static float angleBetween(float yaw, float pitch, float yaw2, float pitch2) {
         float yawDiff = Math.abs(((yaw2 - yaw + 180) % 360) - 180);
         float pitchDiff = Math.abs(pitch2 - pitch);

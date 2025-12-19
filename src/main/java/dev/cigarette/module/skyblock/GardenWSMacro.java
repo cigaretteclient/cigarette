75a1a72c85b2151cbd3c9dffbd785d38a5192550
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-12-18 20:20:41 -0500

    add left/right key holding and secondary delay

--- src/main/java/dev/cigarette/module/skyblock/GardenWSMacro.java
+++ src/main/java/dev/cigarette/module/skyblock/GardenWSMacro.java
@@ -2,9 +2,7 @@ package dev.cigarette.module.skyblock;
 
 import dev.cigarette.Cigarette;
 import dev.cigarette.GameDetector;
-import dev.cigarette.gui.widget.KeybindWidget;
-import dev.cigarette.gui.widget.SliderWidget;
-import dev.cigarette.gui.widget.ToggleWidget;
+import dev.cigarette.gui.widget.*;
 import dev.cigarette.helper.KeybindHelper;
 import dev.cigarette.helper.TickHelper;
 import dev.cigarette.module.TickModule;
@@ -22,6 +20,12 @@ public class GardenWSMacro extends TickModule<ToggleWidget, Boolean> {
     private final SliderWidget switchDelay = new SliderWidget("Switch Delay", "The delay (in ticks) between switching directions once the player stops moving.").withBounds(0, 0, 40);
     private final ToggleWidget holdLeftClick = new ToggleWidget("Hold Left Click", "Holds left click while the macro is running.").withDefaultState(true);
 
+    private final ToggleWidget forwardLeft = new ToggleWidget("Left on Forward", "Holds left while moving forward.").withDefaultState(false);
+    private final ToggleWidget forwardRight = new ToggleWidget("Right on Forward", "Holds right while moving forward.").withDefaultState(false);
+    private final ToggleWidget backLeft = new ToggleWidget("Left on Back", "Holds left while moving forward.").withDefaultState(false);
+    private final ToggleWidget backRight = new ToggleWidget("Right on Back", "Holds right while moving forward.").withDefaultState(false);
+    private final SliderWidget secondaryDelay = new SliderWidget("Hold Delay", "The delay (in ticks) between holding left/right after switching directions.").withBounds(0, 0, 40);
+
     private boolean running = false;
     private boolean isForward = false;
     private boolean paused = false;
@@ -29,11 +33,21 @@ public class GardenWSMacro extends TickModule<ToggleWidget, Boolean> {
 
     private GardenWSMacro(String id, String name, String tooltip) {
         super(ToggleWidget::module, id, name, tooltip);
-        this.setChildren(toggle, switchDelay, holdLeftClick, sendLogs);
+
+        DropdownWidget<TextWidget, BaseWidget.Stateless> config = new DropdownWidget<>("", "");
+        config.setHeader(new TextWidget("Secondary Key", "Configure the left/right movement for the cycle."));
+        config.setChildren(forwardLeft, forwardRight, backLeft, backRight, secondaryDelay);
+
+        this.setChildren(toggle, switchDelay, holdLeftClick, sendLogs, config);
         toggle.registerConfigKey(id + ".toggle");
         switchDelay.registerConfigKey(id + ".switchdelay");
         holdLeftClick.registerConfigKey(id + ".holdleftclick");
         sendLogs.registerConfigKey(id + ".sendlogs");
+        forwardLeft.registerConfigKey(id + ".secondary.forwardleft");
+        forwardRight.registerConfigKey(id + ".secondary.forwardright");
+        backLeft.registerConfigKey(id + ".secondary.backleft");
+        backRight.registerConfigKey(id + ".secondary.backright");
+        secondaryDelay.registerConfigKey(id + ".secondary.delay");
     }
 
     @Override
@@ -46,6 +60,9 @@ public class GardenWSMacro extends TickModule<ToggleWidget, Boolean> {
                 KeybindHelper.KEY_ATTACK.release();
                 KeybindHelper.KEY_MOVE_FORWARD.release();
                 KeybindHelper.KEY_MOVE_BACK.release();
+                KeybindHelper.KEY_MOVE_LEFT.release();
+                KeybindHelper.KEY_MOVE_RIGHT.release();
+                TickHelper.unschedule(this);
             } else {
                 isForward = false;
             }
@@ -71,14 +88,25 @@ public class GardenWSMacro extends TickModule<ToggleWidget, Boolean> {
             if (holdLeftClick.getRawState()) {
                 KeybindHelper.KEY_ATTACK.hold();
             }
-            if (isForward) {
-                KeybindHelper.KEY_MOVE_BACK.release();
-                KeybindHelper.KEY_MOVE_FORWARD.hold(true);
-            } else {
-                KeybindHelper.KEY_MOVE_FORWARD.release();
-                KeybindHelper.KEY_MOVE_BACK.hold(true);
-            }
+
+            KeybindHelper.KEY_MOVE_FORWARD.hold(isForward);
+            KeybindHelper.KEY_MOVE_BACK.hold(!isForward);
+            KeybindHelper.KEY_MOVE_LEFT.release();
+            KeybindHelper.KEY_MOVE_RIGHT.release();
+
             paused = true;
+            if (secondaryDelay.getRawState().intValue() == 0) {
+                KeybindHelper.KEY_MOVE_LEFT.hold((isForward && forwardLeft.getRawState()) || (!isForward && backLeft.getRawState()));
+                KeybindHelper.KEY_MOVE_RIGHT.hold((isForward && forwardRight.getRawState()) || (!isForward && backRight.getRawState()));
+            } else {
+                TickHelper.scheduleOnce(this, () -> {
+                    KeybindHelper.KEY_MOVE_LEFT.hold((isForward && forwardLeft.getRawState()) || (!isForward && backLeft.getRawState()));
+                    KeybindHelper.KEY_MOVE_RIGHT.hold((isForward && forwardRight.getRawState()) || (!isForward && backRight.getRawState()));
+                    paused = false;
+                }, secondaryDelay.getRawState().intValue());
+                return;
+            }
+
             TickHelper.scheduleOnce(this, () -> {
                 paused = false;
             }, 10);

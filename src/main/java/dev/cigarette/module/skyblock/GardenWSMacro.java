69c71e98482efbc8864a7f24f27ba56d2574f7e5
UndercoverGoose <53491740+UndercoverGoose@users.noreply.github.com>
2025-12-19 00:08:49 -0500

    make every single key configurable

--- src/main/java/dev/cigarette/module/skyblock/GardenWSMacro.java
+++ src/main/java/dev/cigarette/module/skyblock/GardenWSMacro.java
@@ -14,40 +14,130 @@ import org.jetbrains.annotations.NotNull;
 
 public class GardenWSMacro extends TickModule<ToggleWidget, Boolean> {
     public static final GardenWSMacro INSTANCE = new GardenWSMacro("skyblock.gardenwsmacro", "Garden WS Macro", "Swaps between holding W and S while farming.");
+    private static final int MAX_TICK_DELAY = 40;
 
     private final KeybindWidget toggle = new KeybindWidget("Toggle Keybind", "A keybind to toggle the macro.");
     private final ToggleWidget sendLogs = new ToggleWidget("Send Chat Logs", "Sends chat logs when switching directions.").withDefaultState(true);
-    private final SliderWidget switchDelay = new SliderWidget("Switch Delay", "The delay (in ticks) between switching directions once the player stops moving.").withBounds(0, 0, 40);
-    private final ToggleWidget holdLeftClick = new ToggleWidget("Hold Left Click", "Holds left click while the macro is running.").withDefaultState(true);
+    private final SliderWidget switchDelay = new SliderWidget("Switch Delay", "The delay (in ticks) between switching directions once the player stops moving.").withBounds(0, 0, MAX_TICK_DELAY);
 
-    private final ToggleWidget forwardLeft = new ToggleWidget("Left on Forward", "Holds left while moving forward.").withDefaultState(false);
-    private final ToggleWidget forwardRight = new ToggleWidget("Right on Forward", "Holds right while moving forward.").withDefaultState(false);
-    private final ToggleWidget backLeft = new ToggleWidget("Left on Back", "Holds left while moving forward.").withDefaultState(false);
-    private final ToggleWidget backRight = new ToggleWidget("Right on Back", "Holds right while moving forward.").withDefaultState(false);
-    private final SliderWidget secondaryDelay = new SliderWidget("Hold Delay", "The delay (in ticks) between holding left/right after switching directions.").withBounds(0, 0, 40);
+    private final ToggleWidget dir1Forward = new ToggleWidget("Hold Forward", "Move forward.").withDefaultState(true);
+    private final SliderWidget dir1ForwardDelay = new SliderWidget("Delay", "The delay (in ticks) before moving forward.").withBounds(0, 0, MAX_TICK_DELAY);
+    private final ToggleWidget dir1Backward = new ToggleWidget("Hold Backward", "Move backward.").withDefaultState(false);
+    private final SliderWidget dir1BackwardDelay = new SliderWidget("Delay", "The delay (in ticks) before moving backward.").withBounds(0, 0, MAX_TICK_DELAY);
+    private final ToggleWidget dir1Left = new ToggleWidget("Hold Left", "Move left.").withDefaultState(false);
+    private final SliderWidget dir1LeftDelay = new SliderWidget("Delay", "The delay (in ticks) before moving left.").withBounds(0, 0, MAX_TICK_DELAY);
+    private final ToggleWidget dir1Right = new ToggleWidget("Hold Right", "Move right.").withDefaultState(false);
+    private final SliderWidget dir1RightDelay = new SliderWidget("Delay", "The delay (in ticks) before moving right.").withBounds(0, 0, MAX_TICK_DELAY);
+    private final ToggleWidget dir1HoldLeft = new ToggleWidget("Hold Left Click", "Hold left click.").withDefaultState(true);
+    private final SliderWidget dir1HoldLeftDelay = new SliderWidget("Delay", "The delay (in ticks) before holding left click.").withBounds(0, 0, MAX_TICK_DELAY);
+
+    private final ToggleWidget dir2Forward = new ToggleWidget("Hold Forward", "Move forward.").withDefaultState(false);
+    private final SliderWidget dir2ForwardDelay = new SliderWidget("Delay", "The delay (in ticks) before moving forward.").withBounds(0, 0, MAX_TICK_DELAY);
+    private final ToggleWidget dir2Backward = new ToggleWidget("Hold Backward", "Move backward.").withDefaultState(true);
+    private final SliderWidget dir2BackwardDelay = new SliderWidget("Delay", "The delay (in ticks) before moving backward.").withBounds(0, 0, MAX_TICK_DELAY);
+    private final ToggleWidget dir2Left = new ToggleWidget("Hold Left", "Move left.").withDefaultState(false);
+    private final SliderWidget dir2LeftDelay = new SliderWidget("Delay", "The delay (in ticks) before moving left.").withBounds(0, 0, MAX_TICK_DELAY);
+    private final ToggleWidget dir2Right = new ToggleWidget("Hold Right", "Move right.").withDefaultState(false);
+    private final SliderWidget dir2RightDelay = new SliderWidget("Delay", "The delay (in ticks) before moving right.").withBounds(0, 0, MAX_TICK_DELAY);
+    private final ToggleWidget dir2HoldLeft = new ToggleWidget("Hold Left Click", "Hold left click.").withDefaultState(true);
+    private final SliderWidget dir2HoldLeftDelay = new SliderWidget("Delay", "The delay (in ticks) before holding left click.").withBounds(0, 0, MAX_TICK_DELAY);
 
     private boolean running = false;
-    private boolean isForward = false;
+    private boolean isPrimary = false;
     private boolean paused = false;
     private boolean wasPaused = false;
 
     private GardenWSMacro(String id, String name, String tooltip) {
         super(ToggleWidget::module, id, name, tooltip);
 
-        DropdownWidget<TextWidget, BaseWidget.Stateless> config = new DropdownWidget<>("", "");
-        config.setHeader(new TextWidget("Secondary Key", "Configure the left/right movement for the cycle."));
-        config.setChildren(forwardLeft, forwardRight, backLeft, backRight, secondaryDelay);
+        DropdownWidget<TextWidget, BaseWidget.Stateless> dir1Config = new DropdownWidget<>("dir1", null);
+        DropdownWidget<TextWidget, BaseWidget.Stateless> dir2Config = new DropdownWidget<>("dir2", null);
 
-        this.setChildren(toggle, switchDelay, holdLeftClick, sendLogs, config);
+        dir1Config.setHeader(new TextWidget("Direction 1", "Configure the movement of the initial/first direction."));
+        dir2Config.setHeader(new TextWidget("Direction 2", "Configure the movement of the alternate/second direction."));
+
+        DropdownWidget<ToggleWidget, Boolean> initialForward = new DropdownWidget<>("a", null);
+        initialForward.setHeader(dir1Forward).setChildren(dir1ForwardDelay);
+        dir1Forward.registerConfigKey(id + ".dir1.forward");
+        dir1ForwardDelay.registerConfigKey(id + ".dir1.forward.delay");
+
+        DropdownWidget<ToggleWidget, Boolean> initialBackward = new DropdownWidget<>("b", null);
+        initialBackward.setHeader(dir1Backward).setChildren(dir1BackwardDelay);
+        dir1Backward.registerConfigKey(id + ".dir1.backward");
+        dir1BackwardDelay.registerConfigKey(id + ".dir1.backward.delay");
+
+        DropdownWidget<ToggleWidget, Boolean> initialLeft = new DropdownWidget<>("c", null);
+        initialLeft.setHeader(dir1Left).setChildren(dir1LeftDelay);
+        dir1Left.registerConfigKey(id + ".dir1.left");
+        dir1LeftDelay.registerConfigKey(id + ".dir1.left.delay");
+
+        DropdownWidget<ToggleWidget, Boolean> initialRight = new DropdownWidget<>("d", null);
+        initialRight.setHeader(dir1Right).setChildren(dir1RightDelay);
+        dir1Right.registerConfigKey(id + ".dir1.right");
+        dir1RightDelay.registerConfigKey(id + ".dir1.right.delay");
+
+        DropdownWidget<ToggleWidget, Boolean> initialHoldLeft = new DropdownWidget<>("e", null);
+        initialHoldLeft.setHeader(dir1HoldLeft).setChildren(dir1HoldLeftDelay);
+        dir1HoldLeft.registerConfigKey(id + ".dir1.holdleft");
+        dir1HoldLeftDelay.registerConfigKey(id + ".dir1.holdleft.delay");
+
+        DropdownWidget<ToggleWidget, Boolean> altForward = new DropdownWidget<>("f", null);
+        altForward.setHeader(dir2Forward).setChildren(dir2ForwardDelay);
+        dir2Forward.registerConfigKey(id + ".dir2.forward");
+        dir2ForwardDelay.registerConfigKey(id + ".dir2.forward.delay");
+
+        DropdownWidget<ToggleWidget, Boolean> altBackward = new DropdownWidget<>("g", null);
+        altBackward.setHeader(dir2Backward).setChildren(dir2BackwardDelay);
+        dir2Backward.registerConfigKey(id + ".dir2.backward");
+        dir2BackwardDelay.registerConfigKey(id + ".dir2.backward.delay");
+
+        DropdownWidget<ToggleWidget, Boolean> altLeft = new DropdownWidget<>("h", null);
+        altLeft.setHeader(dir2Left).setChildren(dir2LeftDelay);
+        dir2Left.registerConfigKey(id + ".dir2.left");
+        dir2LeftDelay.registerConfigKey(id + ".dir2.left.delay");
+
+        DropdownWidget<ToggleWidget, Boolean> altRight = new DropdownWidget<>("i", null);
+        altRight.setHeader(dir2Right).setChildren(dir2RightDelay);
+        dir2Right.registerConfigKey(id + ".dir2.right");
+        dir2RightDelay.registerConfigKey(id + ".dir2.right.delay");
+
+        DropdownWidget<ToggleWidget, Boolean> altHoldLeft = new DropdownWidget<>("j", null);
+        altHoldLeft.setHeader(dir2HoldLeft).setChildren(dir2HoldLeftDelay);
+        dir2HoldLeft.registerConfigKey(id + ".dir2.holdleft");
+        dir2HoldLeftDelay.registerConfigKey(id + ".dir2.holdleft.delay");
+
+        dir1Config.setChildren(initialForward, initialBackward, initialLeft, initialRight, initialHoldLeft);
+        dir2Config.setChildren(altForward, altBackward, altLeft, altRight, altHoldLeft);
+
+        this.setChildren(toggle, switchDelay, sendLogs, dir1Config, dir2Config);
         toggle.registerConfigKey(id + ".toggle");
         switchDelay.registerConfigKey(id + ".switchdelay");
-        holdLeftClick.registerConfigKey(id + ".holdleftclick");
         sendLogs.registerConfigKey(id + ".sendlogs");
-        forwardLeft.registerConfigKey(id + ".secondary.forwardleft");
-        forwardRight.registerConfigKey(id + ".secondary.forwardright");
-        backLeft.registerConfigKey(id + ".secondary.backleft");
-        backRight.registerConfigKey(id + ".secondary.backright");
-        secondaryDelay.registerConfigKey(id + ".secondary.delay");
+    }
+
+    private void reset() {
+        TickHelper.unschedule(this);
+        TickHelper.unschedule(dir1Forward);
+        TickHelper.unschedule(dir1Backward);
+        TickHelper.unschedule(dir1Left);
+        TickHelper.unschedule(dir1Right);
+        TickHelper.unschedule(dir1HoldLeft);
+        TickHelper.unschedule(dir2Forward);
+        TickHelper.unschedule(dir2Backward);
+        TickHelper.unschedule(dir2Left);
+        TickHelper.unschedule(dir2Right);
+        TickHelper.unschedule(dir2HoldLeft);
+        this.releaseAllKeys();
+        isPrimary = true;
+        paused = false;
+    }
+
+    private void releaseAllKeys() {
+        KeybindHelper.KEY_ATTACK.release();
+        KeybindHelper.KEY_MOVE_FORWARD.release();
+        KeybindHelper.KEY_MOVE_BACK.release();
+        KeybindHelper.KEY_MOVE_LEFT.release();
+        KeybindHelper.KEY_MOVE_RIGHT.release();
     }
 
     @Override
@@ -57,14 +147,9 @@ public class GardenWSMacro extends TickModule<ToggleWidget, Boolean> {
             wasPaused = false;
             Cigarette.CHAT_LOGGER.info("Garden WS Macro " + (running ? "Enabled" : "Disabled") + ".");
             if (!running) {
-                KeybindHelper.KEY_ATTACK.release();
-                KeybindHelper.KEY_MOVE_FORWARD.release();
-                KeybindHelper.KEY_MOVE_BACK.release();
-                KeybindHelper.KEY_MOVE_LEFT.release();
-                KeybindHelper.KEY_MOVE_RIGHT.release();
-                TickHelper.unschedule(this);
+                this.reset();
             } else {
-                isForward = false;
+                isPrimary = false;
             }
             return;
         }
@@ -84,32 +169,65 @@ public class GardenWSMacro extends TickModule<ToggleWidget, Boolean> {
                 return;
             }
             wasPaused = false;
-            isForward = !isForward;
-            if (holdLeftClick.getRawState()) {
-                KeybindHelper.KEY_ATTACK.hold();
+            isPrimary = !isPrimary;
+
+            ToggleWidget forward = isPrimary ? dir1Forward : dir2Forward;
+            SliderWidget forwardDelay = isPrimary ? dir1ForwardDelay : dir2ForwardDelay;
+            ToggleWidget backward = isPrimary ? dir1Backward : dir2Backward;
+            SliderWidget backwardDelay = isPrimary ? dir1BackwardDelay : dir2BackwardDelay;
+            ToggleWidget left = isPrimary ? dir1Left : dir2Left;
+            SliderWidget leftDelay = isPrimary ? dir1LeftDelay : dir2LeftDelay;
+            ToggleWidget right = isPrimary ? dir1Right : dir2Right;
+            SliderWidget rightDelay = isPrimary ? dir1RightDelay : dir2RightDelay;
+            ToggleWidget holdLeft = isPrimary ? dir1HoldLeft : dir2HoldLeft;
+            SliderWidget holdLeftDelay = isPrimary ? dir1HoldLeftDelay : dir2HoldLeftDelay;
+
+            this.releaseAllKeys();
+
+            if (forwardDelay.getRawState() == 0) {
+                KeybindHelper.KEY_MOVE_FORWARD.hold(forward.getRawState());
+            } else {
+                TickHelper.scheduleOnce(forward, () -> {
+                    KeybindHelper.KEY_MOVE_FORWARD.hold(forward.getRawState());
+                }, forwardDelay.getRawState().intValue());
             }
 
-            KeybindHelper.KEY_MOVE_FORWARD.hold(isForward);
-            KeybindHelper.KEY_MOVE_BACK.hold(!isForward);
-            KeybindHelper.KEY_MOVE_LEFT.release();
-            KeybindHelper.KEY_MOVE_RIGHT.release();
+            if (backwardDelay.getRawState() == 0) {
+                KeybindHelper.KEY_MOVE_BACK.hold(backward.getRawState());
+            } else {
+                TickHelper.scheduleOnce(backward, () -> {
+                    KeybindHelper.KEY_MOVE_BACK.hold(backward.getRawState());
+                }, backwardDelay.getRawState().intValue());
+            }
+
+            if (leftDelay.getRawState() == 0) {
+                KeybindHelper.KEY_MOVE_LEFT.hold(left.getRawState());
+            } else {
+                TickHelper.scheduleOnce(left, () -> {
+                    KeybindHelper.KEY_MOVE_LEFT.hold(left.getRawState());
+                }, leftDelay.getRawState().intValue());
+            }
+
+            if (rightDelay.getRawState() == 0) {
+                KeybindHelper.KEY_MOVE_RIGHT.hold(right.getRawState());
+            } else {
+                TickHelper.scheduleOnce(right, () -> {
+                    KeybindHelper.KEY_MOVE_RIGHT.hold(right.getRawState());
+                }, rightDelay.getRawState().intValue());
+            }
+
+            if (holdLeftDelay.getRawState() == 0) {
+                KeybindHelper.KEY_ATTACK.hold(holdLeft.getRawState());
+            } else {
+                TickHelper.scheduleOnce(holdLeft, () -> {
+                    KeybindHelper.KEY_ATTACK.hold(holdLeft.getRawState());
+                }, holdLeftDelay.getRawState().intValue());
+            }
 
             paused = true;
-            if (secondaryDelay.getRawState().intValue() == 0) {
-                KeybindHelper.KEY_MOVE_LEFT.hold((isForward && forwardLeft.getRawState()) || (!isForward && backLeft.getRawState()));
-                KeybindHelper.KEY_MOVE_RIGHT.hold((isForward && forwardRight.getRawState()) || (!isForward && backRight.getRawState()));
-            } else {
-                TickHelper.scheduleOnce(this, () -> {
-                    KeybindHelper.KEY_MOVE_LEFT.hold((isForward && forwardLeft.getRawState()) || (!isForward && backLeft.getRawState()));
-                    KeybindHelper.KEY_MOVE_RIGHT.hold((isForward && forwardRight.getRawState()) || (!isForward && backRight.getRawState()));
-                    paused = false;
-                }, secondaryDelay.getRawState().intValue());
-                return;
-            }
-
             TickHelper.scheduleOnce(this, () -> {
                 paused = false;
-            }, 10);
+            }, MAX_TICK_DELAY);
         }
     }
 
